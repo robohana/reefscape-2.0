5b7694ddb91aee2fa696b0a53e28ab7cc0c5fc05
Jesse Lard <jesselard@gmail.com>
2025-03-02 12:06:33 -0600

    Update MAX_swerve_module.py

--- subsystems/MAX_swerve_module.py
+++ subsystems/MAX_swerve_module.py
@@ -4,7 +4,7 @@ import commands2
 from wpimath.controller import PIDController, SimpleMotorFeedforwardMeters, SimpleMotorFeedforwardRadians
 from constants import DriveConstants
 
-from rev import SparkMax, SparkMaxConfig, SparkBase, SparkBaseConfig
+from rev import SparkMax, SparkMaxConfig, SparkBase, SparkBaseConfig, ClosedLoopConfig, ClosedLoopSlot
 
 from wpimath.geometry import Rotation2d
 from wpimath.kinematics import SwerveModuleState, SwerveModulePosition
@@ -24,34 +24,34 @@ class MAXSwerveModule(commands2.SubsystemBase):
         self.driveMotor = SparkMax(driveMotorChannel, SparkMax.MotorType.kBrushless)
         self.turningMotor = SparkMax(turningMotorChannel, SparkMax.MotorType.kBrushless)
 
-        driveMotorConfig = SparkMaxConfig()
-        turnMotorConfig = SparkMaxConfig()
-
-        driveMotorConfig.encoder.positionConversionFactor(DriveConstants.K_DRIVE_ENCODER_ROT2METER)
-        driveMotorConfig.encoder.velocityConversionFactor(DriveConstants.K_DRIVE_ENCODER_RPM2METER_PER_SEC) 
-
-        turnMotorConfig.encoder.positionConversionFactor(DriveConstants.K_TURN_ENCODER_ROT2RAD)
-        turnMotorConfig.encoder.velocityConversionFactor(DriveConstants.K_TURN_ENCODER_RPM2RAD_PER_SEC)
-
-        self.driveMotor.configure(driveMotorConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
-        self.turningMotor.configure(turnMotorConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
-
-        # Setup encoders for the drive and turning motors.
+            # Setup encoders for the drive and turning motors.
         self.driveEncoder = self.driveMotor.getEncoder()
         self.turningEncoder = self.turningMotor.getAbsoluteEncoder()
 
+        driveMotorConfig = SparkMaxConfig()
+        turnMotorConfig = SparkMaxConfig()
 
-        # Setup PID controllers for the driving and turning SPARKS MAX.
-        self.drivePIDController = self.driveMotor.getClosedLoopController()
-        self.turningPIDController = PIDController(DriveConstants.K_TURN_P, DriveConstants.K_TURN_I, DriveConstants.K_TURN_D)
-        self.turningPIDController.enableContinuousInput(-math.pi, math.pi)
+        driveMotorConfig.setIdleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(50)
+        driveMotorConfig.encoder.positionConversionFactor(DriveConstants.K_DRIVE_ENCODER_ROT2METER) # Meters
+        driveMotorConfig.encoder.velocityConversionFactor(DriveConstants.K_DRIVE_ENCODER_ROT2METER / 60.0) # Meters per second
+        driveMotorConfig.closedLoop.setFeedbackSensor(ClosedLoopConfig.FeedbackSensor.kPrimaryEncoder)
+        driveMotorConfig.closedLoop.pid(DriveConstants.K_DRIVE_P, DriveConstants.K_DRIVE_I, DriveConstants.K_DRIVE_D)
+        driveMotorConfig.closedLoop.velocityFF(DriveConstants.K_DRIVE_KS, ClosedLoopSlot.kSlot0) # Wants ff:The velocity feedforward gain value, slot: The closed loop slot to set the values for. 
+                                            # set to slot 0 because that is the default
+        driveMotorConfig.closedLoop.outputRange(-1, 1)
 
-        self.driveFeedforward = SimpleMotorFeedforwardMeters(DriveConstants.K_DRIVE_KS, DriveConstants.K_DRIVE_KV)
-        self.turnFeedforward = SimpleMotorFeedforwardRadians(DriveConstants.K_TURN_KS, DriveConstants.K_TURN_KV)
+        turnMotorConfig.setIdleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(20)
+        turnMotorConfig.absoluteEncoder.positionConversionFactor(DriveConstants.K_TURN_ENCODER_ROT2RAD)
+        turnMotorConfig.absoluteEncoder.velocityConversionFactor(DriveConstants.K_TURN_ENCODER_ROT2RAD / 60.0)
+        #Invert the turning encoder, since the output shaft rotates in the opposite direction of the steering motor in the MAXSwerve Module.
+        turnMotorConfig.absoluteEncoder.inverted(True)  
+        turnMotorConfig.closedLoop.setFeedbackSensor(ClosedLoopConfig.FeedbackSensor.kAbsoluteEncoder)
+        turnMotorConfig.closedLoop.pid(DriveConstants.K_TURN_P, DriveConstants.K_TURN_I, DriveConstants.K_TURN_D)
+        turnMotorConfig.closedLoop.outputRange(-1, 1)
+        #Enable PID wrap around for the turning motor. This will allow the PID controller to go through 0 to get to the setpoint i.e. going from 350 degrees to 10 degrees will go through 0 rather than the other direction which is a longer route.
+        turnMotorConfig.closedLoop.positionWrappingEnabled(True) # I don't know if this is a correct value - LC 2/23/25
+        turnMotorConfig.closedLoop.positionWrappingInputRange(0, DriveConstants.K_TURN_ENCODER_ROT2RAD) 
 
-        # Set the idle mode for the motors to brake.
-        driveMotorConfig.setIdleMode(SparkBaseConfig.IdleMode.kBrake)
-        turnMotorConfig.setIdleMode(SparkBaseConfig.IdleMode.kBrake)
 
         self.desiredState = SwerveModuleState()
         self.chassis_angular_offset = chassis_angular_offset
@@ -71,6 +71,7 @@ class MAXSwerveModule(commands2.SubsystemBase):
             self.driveEncoder.getPosition(),
             Rotation2d(self.turningEncoder.getPosition() - self.chassis_angular_offset)
         )
+    
  
 
     def setDesiredState(self, desiredState: SwerveModuleState):
@@ -79,22 +80,9 @@ class MAXSwerveModule(commands2.SubsystemBase):
         correctedDesiredState.speed = desiredState.speed
         correctedDesiredState.angle = desiredState.angle + Rotation2d(self.chassis_angular_offset)
         correctedDesiredState.optimize(Rotation2d(self.turningEncoder.getPosition()))
-        if math.isclose(desiredState.speed, 0):
-            self.driveMotor.stopMotor()
-        else:
-            self.drivePIDController.setReference(correctedDesiredState.speed, SparkBase.ControlType.kVelocity)
-
-        try:
-            # For the turning motor, we typically use PID control.
-            turningOutput = self.turningPIDController.calculate(self.getState().angle, correctedDesiredState.angle.radians())
-            self.turningMotor.set(turningOutput)
-        except Exception as e:
-            print(f"Turning motor set error: {e}")
-            self.turningMotor.set(0)
 
         
-        self.desiredState = correctedDesiredState
-
+        self.desiredState = desiredState
 
     def stop(self):
         """Stops the module."""

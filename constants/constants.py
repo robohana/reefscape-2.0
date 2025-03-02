b88a5c4f394a7c7f532452e1dd248816e819028e
Liliana <lilianacarlson.lrc@gmail.com>
2025-03-02 13:09:39 -0600

    Update constants.py

--- constants/constants.py
+++ constants/constants.py
@@ -40,30 +40,36 @@ class DriveConstants:
     K_TURN_KS = 0.1
     K_TURN_KV = 0.227
 
+    kDrivingMotorPinionTeeth = 14 
 
-    K_WHEEL_DIAMETER_METERS = units.inchesToMeters(4) # ~ 0.1016 # wheel listed as "Wheel, Billet, 4"OD x 1.5"W (MK4/4i)""
+    K_WHEEL_DIAMETER_METERS = 0.0762
     K_WHEEL_RADIUS_METERS = K_WHEEL_DIAMETER_METERS / 2 # ~ 0.0508
+    kWheelCircumferenceMeters = K_WHEEL_DIAMETER_METERS * pi
 
-    K_DRIVE_MOTOR_GEAR_RATIO = 1 / 6.12  
-    K_TURN_ENCODER_GEAR_RATIO = 1 / (150 / 7)  
+   
+    kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15)
+    kFreeSpeedRpm = 5676
+    kDrivingMotorFreeSpeedRps = kFreeSpeedRpm / 60
+
+    kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters) / kDrivingMotorReduction   
+    
     # I don't know what to call these yet
     # Conversion from encoder rotations to meters (drive)
-    K_DRIVE_ENCODER_ROT2METER = K_DRIVE_MOTOR_GEAR_RATIO * pi * K_WHEEL_DIAMETER_METERS
+    K_DRIVE_ENCODER_ROT2METER = kDrivingMotorReduction * pi * K_WHEEL_DIAMETER_METERS
 
     # Conversion from encoder rotations to radians (turn)
-    K_TURN_ENCODER_ROT2RAD = K_TURN_ENCODER_GEAR_RATIO * pi * 2  
+    # K_TURN_ENCODER_ROT2RAD = K_TURN_ENCODER_GEAR_RATIO * pi * 2  
 
     # Conversion from encoder RPM to meters per second (drive)
     K_DRIVE_ENCODER_RPM2METER_PER_SEC = K_DRIVE_ENCODER_ROT2METER / 60
 
     # Conversion from encoder RPM to radians per second (turn)
-    K_TURN_ENCODER_RPM2RAD_PER_SEC = K_TURN_ENCODER_ROT2RAD / 60
+    # K_TURN_ENCODER_RPM2RAD_PER_SEC = K_TURN_ENCODER_ROT2RAD / 60
 
 
     # Movement Constants
     # Physical robot movement limits (in feet and meters)
-    K_PHYSICAL_MAX_SPEED_FEET_PER_SECOND = 16.6  # ft/s from SwerveSpecialties website
-    K_PHYSICAL_MAX_SPEED_METERS_PER_SECOND = K_PHYSICAL_MAX_SPEED_FEET_PER_SECOND / 3.281  # ~5.05968 m/s
+    K_PHYSICAL_MAX_SPEED_METERS_PER_SECOND = 4.8
 
     # Physical max angular velocity (in meters per second)
     K_PHYSICAL_MAX_ANGULAR_VELOCITY_METERS_PER_SECOND = K_PHYSICAL_MAX_SPEED_METERS_PER_SECOND / K_WHEEL_RADIUS_METERS  # rad/s
@@ -88,46 +94,39 @@ class OIConstants:
 class RobotConstants:
 
         # Robot Dimemsions
-    K_TRACK_WIDTH = units.inchesToMeters(23) # ~0.584m
+    K_TRACK_WIDTH = units.inchesToMeters(23.375) # ~0.584m
      # Distance between centers of right and left wheels
-    K_WHEEL_BASE = units.inchesToMeters(23) # ~0.584m
+    K_WHEEL_BASE = units.inchesToMeters(23.375) # ~0.584m
      # Distance between centers of front and back wheels
 
 
     K_GYRO_REVERSED = False
 
-    # Sensor Constants
-    K_ENCODER_RESOLUTION = 4096 # from thrifty bot abs enc user manual
-    
     
     # Front left module constants
-    K_FRONT_LEFT_DRIVE_ID = 20
-    K_FRONT_LEFT_TURN_ID = 31
-    K_FRONT_LEFT_ABSOLUTE_ENCODER_ID = 3
+    K_FRONT_LEFT_DRIVE_ID = 23
+    K_FRONT_LEFT_TURN_ID = 22
     K_FRONT_LEFT_DRIVING_MOTOR_REVERSED = False
     K_FRONT_LEFT_TURNING_MOTOR_REVERSED = False
     K_FRONT_LEFT_CHASSIS_ANGULAR_OFFSET = pi / 4
 
     # Front right module constants
-    K_FRONT_RIGHT_DRIVE_ID = 23
-    K_FRONT_RIGHT_TURN_ID = 22
-    K_FRONT_RIGHT_ABSOLUTE_ENCODER_ID = 0
+    K_FRONT_RIGHT_DRIVE_ID = 21
+    K_FRONT_RIGHT_TURN_ID = 20
     K_FRONT_RIGHT_DRIVING_MOTOR_REVERSED = True
     K_FRONT_RIGHT_TURNING_MOTOR_REVERSED = False
     K_FRONT_RIGHT_CHASSIS_ANGULAR_OFFSET = -pi / 4
 
     # Back left module constants
-    K_BACK_LEFT_DRIVE_ID = 24
-    K_BACK_LEFT_TURN_ID = 30
-    K_BACK_LEFT_ABSOLUTE_ENCODER_ID = 2
+    K_BACK_LEFT_DRIVE_ID = 25
+    K_BACK_LEFT_TURN_ID = 24
     K_BACK_LEFT_DRIVING_MOTOR_REVERSED = False
     K_BACK_LEFT_TURNING_MOTOR_REVERSED = False
     K_BACK_LEFT_CHASSIS_ANGULAR_OFFSET = (3 * pi) / 4
 
     # Back right module constants
-    K_BACK_RIGHT_DRIVE_ID = 21
-    K_BACK_RIGHT_TURN_ID = 29
-    K_BACK_RIGHT_ABSOLUTE_ENCODER_ID = 1
+    K_BACK_RIGHT_DRIVE_ID = 27
+    K_BACK_RIGHT_TURN_ID = 26
     K_BACK_RIGHT_DRIVING_MOTOR_REVERSED = True
     K_BACK_RIGHT_TURNING_MOTOR_REVERSED = False
     K_BACK_RIGHT_CHASSIS_ANGULAR_OFFSET = -(3 * pi) / 4
@@ -154,4 +153,3 @@ class AutoConstants:
     kMaxAngularSpeedRadiansPerSecond = pi
     kMaxAngularSpeedRadiansPerSecondSquared = pi
     
-

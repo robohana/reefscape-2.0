a74599237df90fd83d01c94eec948e0ae7e74efd
Liliana <lilianacarlson.lrc@gmail.com>
2025-03-02 14:38:25 -0600

    Update robot_container.py

--- robot_container.py
+++ robot_container.py
@@ -14,7 +14,7 @@ from commands2 import Command, CommandScheduler, cmd, InstantCommand, RunCommand
 import wpimath
 import wpilib
 
-
+from wpimath.filter import SlewRateLimiter
 from wpimath.controller import PIDController, ProfiledPIDControllerRadians
 from wpimath.geometry import Pose2d, Rotation2d, Translation2d
 from wpimath.trajectory import (
@@ -45,6 +45,7 @@ class RobotContainer:
 
     def __init__(self) -> None:
 
+
         self.driver_controller = wpilib.XboxController(OIConstants.kDriverControllerPort)
         self.operatorController = wpilib.XboxController(OIConstants.kOperatorControllerPort)
         # The robot's subsystems
@@ -67,12 +68,13 @@ class RobotContainer:
         # Configure the button bindings
 
     def setDefaultCommand (self) -> None:
+
         self.robot_drive.setDefaultCommand(
             RunCommand(
                 lambda: self.robot_drive.drive(
-                    -wpimath.applyDeadband(self.driver_controller.getLeftY(), OIConstants.deadzone),
-                    -wpimath.applyDeadband(self.driver_controller.getLeftX(), OIConstants.deadzone),
-                    -wpimath.applyDeadband(self.driver_controller.getRightX(), OIConstants.deadzone),
+                    wpimath.applyDeadband(self.driver_controller.getLeftY() * -1, OIConstants.deadzone),
+                    wpimath.applyDeadband(self.driver_controller.getLeftX() * -1, OIConstants.deadzone),
+                    wpimath.applyDeadband(self.driver_controller.getRightX() * -1, OIConstants.deadzone),
                     True
                 ),
                 self.robot_drive 

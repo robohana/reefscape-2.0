94dcebcdcab633747e6fe49956aa6d5d56425098
Liliana <lilianacarlson.lrc@gmail.com>
2025-03-02 12:24:14 -0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,96 +1,53 @@
-import math
-import commands2
-
-from wpimath.controller import PIDController, SimpleMotorFeedforwardMeters, SimpleMotorFeedforwardRadians
-from constants import DriveConstants
-
-from rev import SparkMax, SparkMaxConfig, SparkBase, SparkBaseConfig, ClosedLoopConfig, ClosedLoopSlot, SparkClosedLoopController
-
-from wpimath.geometry import Rotation2d
-from wpimath.kinematics import SwerveModuleState, SwerveModulePosition
-from ntcore import NetworkTableInstance
+import rev
+# import math
+from rev import ClosedLoopConfig, ClosedLoopSlot, SparkMaxConfig, SparkBaseConfig, SparkFlexConfig
+from math import pi
+from constants.module_constants import ModuleConstants
+from constants.drive_constants import DriveConstants
 
 
-class MAXSwerveModule(commands2.SubsystemBase):
-    def __init__(self, driveMotorChannel: int, turningMotorChannel: int,  chassis_angular_offset: float) -> None:
-        """Constructs a SwerveModule with a drive motor, turning motor, drive encoder, and turning encoder."""
-        super().__init__()
-        # Force NetworkTables to start
-        self.nt = NetworkTableInstance.getDefault()
-        self.debugTable = self.nt.getTable("SwerveDebug")
-        print("NetworkTables started: SwerveDebug should be available")
 
+class MAXSwerveModule:
+    drive_config = SparkMaxConfig()
+    turn_config = SparkMaxConfig()
 
-        self.driveMotor = SparkMax(driveMotorChannel, SparkMax.MotorType.kBrushless)
-        self.turningMotor = SparkMax(turningMotorChannel, SparkMax.MotorType.kBrushless)
+    drive_config.setIdleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(50)
+    drive_config.encoder.positionConversionFactor(DriveConstants.K_DRIVE_ENCODER_ROT2METER) # Meters
+    drive_config.encoder.velocityConversionFactor(DriveConstants.K_DRIVE_ENCODER_ROT2METER / 60.0) # Meters per second
+    drive_config.closedLoop.setFeedbackSensor(ClosedLoopConfig.FeedbackSensor.kPrimaryEncoder)
+    drive_config.closedLoop.pid(DriveConstants.K_DRIVE_P, DriveConstants.K_DRIVE_I, DriveConstants.K_DRIVE_D)
+    drive_config.closedLoop.velocityFF(DriveConstants.K_DRIVE_KS, ClosedLoopSlot.kSlot0) # Wants ff:The velocity feedforward gain value, slot: The closed loop slot to set the values for. 
+                                        # set to slot 0 because that is the default
+    drive_config.closedLoop.outputRange(-1, 1)
 
-            # Setup encoders for the drive and turning motors.
-        self.driveEncoder = self.driveMotor.getEncoder()
-        self.turningEncoder = self.turningMotor.getAbsoluteEncoder()
+    turn_config.setIdleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(20)
+    turn_config.absoluteEncoder.positionConversionFactor(DriveConstants.K_TURN_ENCODER_ROT2RAD)
+    turn_config.absoluteEncoder.velocityConversionFactor(DriveConstants.K_TURN_ENCODER_ROT2RAD / 60.0)
+    #Invert the turning encoder, since the output shaft rotates in the opposite direction of the steering motor in the MAXSwerve Module.
+    turn_config.absoluteEncoder.inverted(True)  
+    turn_config.closedLoop.setFeedbackSensor(ClosedLoopConfig.FeedbackSensor.kAbsoluteEncoder)
+    turn_config.closedLoop.pid(DriveConstants.K_TURN_P, DriveConstants.K_TURN_I, DriveConstants.K_TURN_D)
+    turn_config.closedLoop.outputRange(-1, 1)
+    #Enable PID wrap around for the turning motor. This will allow the PID controller to go through 0 to get to the setpoint i.e. going from 350 degrees to 10 degrees will go through 0 rather than the other direction which is a longer route.
+    turn_config.closedLoop.positionWrappingEnabled(True) # I don't know if this is a correct value - LC 2/23/25
+    turn_config.closedLoop.positionWrappingInputRange(0, DriveConstants.K_TURN_ENCODER_ROT2RAD)        
 
-        self.drivingClosedLoopController = self.driveMotor.getClosedLoopController()
-        self.turningClosedLoopController = self.turningMotor.getClosedLoopController()
+class CoralSubsytem:
+    arm_config = SparkMaxConfig()
+    elevator_config = SparkMaxConfig()
+    intake_config = SparkMaxConfig()
 
-        driveMotorConfig = SparkMaxConfig()
-        turnMotorConfig = SparkMaxConfig()
+    arm_config.setIdleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12)
+    # fill in - LC 2/23/25
 
-        driveMotorConfig.setIdleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(50)
-        driveMotorConfig.encoder.positionConversionFactor(DriveConstants.K_DRIVE_ENCODER_ROT2METER) # Meters
-        driveMotorConfig.encoder.velocityConversionFactor(DriveConstants.K_DRIVE_ENCODER_ROT2METER / 60.0) # Meters per second
-        driveMotorConfig.closedLoop.setFeedbackSensor(ClosedLoopConfig.FeedbackSensor.kPrimaryEncoder)
-        driveMotorConfig.closedLoop.pid(DriveConstants.K_DRIVE_P, DriveConstants.K_DRIVE_I, DriveConstants.K_DRIVE_D)
-        driveMotorConfig.closedLoop.velocityFF(DriveConstants.K_DRIVE_KS, ClosedLoopSlot.kSlot0) # Wants ff:The velocity feedforward gain value, slot: The closed loop slot to set the values for. 
-                                            # set to slot 0 because that is the default
-        driveMotorConfig.closedLoop.outputRange(-1, 1)
+    elevator_config.setIdleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(50).voltageCompensation(12)
+    # fill in - LC 2/23/25
 
-        turnMotorConfig.setIdleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(20)
-        turnMotorConfig.absoluteEncoder.positionConversionFactor(DriveConstants.K_TURN_ENCODER_ROT2RAD)
-        turnMotorConfig.absoluteEncoder.velocityConversionFactor(DriveConstants.K_TURN_ENCODER_ROT2RAD / 60.0)
-        #Invert the turning encoder, since the output shaft rotates in the opposite direction of the steering motor in the MAXSwerve Module.
-        turnMotorConfig.absoluteEncoder.inverted(True)  
-        turnMotorConfig.closedLoop.setFeedbackSensor(ClosedLoopConfig.FeedbackSensor.kAbsoluteEncoder)
-        turnMotorConfig.closedLoop.pid(DriveConstants.K_TURN_P, DriveConstants.K_TURN_I, DriveConstants.K_TURN_D)
-        turnMotorConfig.closedLoop.outputRange(-1, 1)
-        #Enable PID wrap around for the turning motor. This will allow the PID controller to go through 0 to get to the setpoint i.e. going from 350 degrees to 10 degrees will go through 0 rather than the other direction which is a longer route.
-        turnMotorConfig.closedLoop.positionWrappingEnabled(True) # I don't know if this is a correct value - LC 2/23/25
-        turnMotorConfig.closedLoop.positionWrappingInputRange(0, DriveConstants.K_TURN_ENCODER_ROT2RAD)
+    intake_config.setIdleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(40)
+    # fill in - LC 2/23/25
 
-        self.driveMotor.configure(driveMotorConfig)
-        self.turningMotor.configure(turnMotorConfig) 
-
-
-        self.desiredState = SwerveModuleState()
-        self.chassis_angular_offset = chassis_angular_offset
-        self.desiredState.angle = Rotation2d(self.turningEncoder.getPosition())
-        self.driveEncoder.setPosition(0)
-
-
-    def getState(self) -> SwerveModuleState:
-        """Returns the current state of the module."""
-        return SwerveModuleState(self.driveEncoder.getVelocity(), Rotation2d(self.turningEncoder.getPosition() - self.chassis_angular_offset))
     
-    def get_position(self):
-        return SwerveModulePosition(
-            self.driveEncoder.getPosition(),
-            Rotation2d(self.turningEncoder.getPosition() - self.chassis_angular_offset)
-        )
-    
- 
 
-    def setDesiredState(self, desiredState: SwerveModuleState):
-        """Sets the desired state of the module, using PID and feedforward for the drive motor."""
-        correctedDesiredState = SwerveModuleState()
-        correctedDesiredState.speed = desiredState.speed
-        correctedDesiredState.angle = desiredState.angle + Rotation2d(self.chassis_angular_offset)
-        correctedDesiredState.optimize(Rotation2d(self.turningEncoder.getPosition()))
+# Put other subsystem configs down here like Coral or Hang - LC 2/23/25    
 
-        self.drivingClosedLoopController.setReference(correctedDesiredState.speed, SparkBase.ControlType.kVelocity)
-        self.turningClosedLoopController.setReference(correctedDesiredState.angle.radians(), SparkBase.ControlType.kPosition)
-        
-        self.desiredState = desiredState
-
-    def stop(self):
-        """Stops the module."""
-        self.driveMotor.stopMotor()
-        self.turningMotor.stopMotor()
-        
+     

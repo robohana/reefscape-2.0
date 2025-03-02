78e465d63feac33c8f34866bead513e326da107b
Jesse Lard <jesselard@gmail.com>
2025-03-02 12:24:05 -0600

    Update MAX_swerve_module.py

--- subsystems/MAX_swerve_module.py
+++ subsystems/MAX_swerve_module.py
@@ -53,17 +53,17 @@ class MAXSwerveModule(commands2.SubsystemBase):
         turnMotorConfig.closedLoop.outputRange(-1, 1)
         #Enable PID wrap around for the turning motor. This will allow the PID controller to go through 0 to get to the setpoint i.e. going from 350 degrees to 10 degrees will go through 0 rather than the other direction which is a longer route.
         turnMotorConfig.closedLoop.positionWrappingEnabled(True) # I don't know if this is a correct value - LC 2/23/25
-        turnMotorConfig.closedLoop.positionWrappingInputRange(0, DriveConstants.K_TURN_ENCODER_ROT2RAD) 
+        turnMotorConfig.closedLoop.positionWrappingInputRange(0, DriveConstants.K_TURN_ENCODER_ROT2RAD)
+
+        self.driveMotor.configure(driveMotorConfig)
+        self.turningMotor.configure(turnMotorConfig) 
 
 
         self.desiredState = SwerveModuleState()
         self.chassis_angular_offset = chassis_angular_offset
         self.desiredState.angle = Rotation2d(self.turningEncoder.getPosition())
-        self.resetEncoders()
-
-    def resetEncoders(self):
-        """Resets the drive encoder to zero."""
         self.driveEncoder.setPosition(0)
+
 
     def getState(self) -> SwerveModuleState:
         """Returns the current state of the module."""

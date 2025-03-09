dcc0d289f5936c8e0f5bcc803fc0f839c0231c48
Liliana <lilianacarlson.lrc@gmail.com>
2025-03-08 20:26:57 -0600

    Update robot_container.py

--- robot_container.py
+++ robot_container.py
@@ -1,29 +1,22 @@
 # Type: RobotContainer
+
 '''
 # The RobotContainer is where you will configure your robot. The container is where you will define your subsystems, commands, and button bindings. 
-#The container is created in robot.py and passed to the TimedCommandRobot. 
-#The TimedCommandRobot will call the getAutonomousCommand# Type: RobotContainer
-'''
-# The RobotContainer is where you will configure your robot. The container is where you will define your subsystems, commands, and button bindings. 
-#The container is created in robot.py and passed to the TimedCommandRobot. 
-#The TimedCommandRobot will call the getAutonomousCommand method to get the command to run during autonomous. 
-#The container will also set the default commands for the subsystems
+# The container is created in robot.py and passed to the TimedCommandRobot. 
+# The TimedCommandRobot will call the getAutonomousCommand method to get the command to run during autonomous. 
+# The container will also set the default commands for the subsystems
 
  '''
 
-
-import commands2
-from commands2 import Command, CommandScheduler, cmd, InstantCommand, RunCommand, button
-import wpimath
-import wpilib
-from wpilib import XboxController
-
+from commands2 import RunCommand, Command
+from commands2.button import CommandXboxController
+from wpimath import applyDeadband
 from subsystems.coral_subsystem import CoralSubsystem
-from constants import AutoConstants, OIConstants, Setpoint
+from subsystems.hang_subsystem import HangSubsystem
 from subsystems.drivetrain import DriveSubsystem
+from constants import AutoConstants, OIConstants, Setpoint
 from commands.SwerveJoystickCmd import SwerveJoystickCmd
-
-import commands.auto_routines
+from commands.auto_routines import SimpleAuto
 
 
 class RobotContainer:
@@ -36,15 +29,19 @@ class RobotContainer:
 
     def __init__(self) -> None:
 
-
-        self.driver_controller = wpilib.XboxController(OIConstants.K_DRIVER_CONTROLLER_PORT)
-        self.operator_controller = wpilib.XboxController(OIConstants.K_OPERATOR_CONTROLLER_PORT)
         # The robot's subsystems
         self.robot_drive = DriveSubsystem()
         self.coral = CoralSubsystem()
-        self.set_default_command()
-        self.configure_button_bindings()
+        self.hang = HangSubsystem()
 
+        # Controllers
+        self.driver_controller = CommandXboxController(OIConstants.K_DRIVER_CONTROLLER_PORT)
+        self.operator_controller = CommandXboxController(OIConstants.K_OPERATOR_CONTROLLER_PORT)  
+
+        # Configure default commands
+        self.set_default_command()
+        # Configure the button bindings
+        self.configure_button_bindings()
 
         # self.robotDrive.setDefaultCommand(
         #     SwerveJoystickCmd(self.robotDrive, self.driverController)
@@ -60,13 +57,15 @@ class RobotContainer:
         # Configure the button bindings
 
     def set_default_command (self) -> None:
+        # The left stick controlls translantion of the robot.
+        # Turning is controled by the X axis of the right stick
 
         self.robot_drive.setDefaultCommand(
             RunCommand(
                 lambda: self.robot_drive.drive(
-                    wpimath.applyDeadband(self.driver_controller.getLeftY() * -1, OIConstants.DEADZONE),
-                    wpimath.applyDeadband(self.driver_controller.getLeftX() * -1, OIConstants.DEADZONE),
-                    wpimath.applyDeadband(self.driver_controller.getRightX() * -1, OIConstants.DEADZONE),
+                    applyDeadband(self.driver_controller.getLeftY() * -1, OIConstants.DEADZONE),
+                    applyDeadband(self.driver_controller.getLeftX() * -1, OIConstants.DEADZONE),
+                    applyDeadband(self.driver_controller.getRightX() * -1, OIConstants.DEADZONE),
                     True
                 ),
                 self.robot_drive 
@@ -81,111 +80,31 @@ class RobotContainer:
         and then passing it to a JoystickButton.
         # """
         # self.robotDrive.setDefaultCommand(SwerveJoystickCmd(self.robotDrive, self.driverController))
-        if self.operator_controller.getLeftBumper(): CoralSubsystem.run_intake_command()
-        if self.operator_controller.getRightBumper(): CoralSubsystem.reverse_intake_command()   
-
-        if self.operator_controller.getBButton(): CoralSubsystem.setSetpointCommand(Setpoint.K_CORAL_STATION)
-        if self.operator_controller.getAButton(): CoralSubsystem.setSetpointCommand(Setpoint.K_LEVEL_2) 
-        if self.operator_controller.getYButton(): CoralSubsystem.setSetpointCommand(Setpoint.K_LEVEL_3) 
-
-        # if self.driver_controller.start(): self.robot_drive.zero_heading
-
-
-    def disablePIDSubsystems(self) -> None:
-        """Disables all ProfiledPIDSubsystem and PIDSubsystem instances.
-        This should be called on robot disable to prevent integral windup."""
-        pass
-
-    def getAutonomousCommand(self) -> commands2.Command:
-        """Use this to pass the autonomous command to the main {@link Robot} class.
-
-        :returns: the command to run in autonomous
-        """
-        pass
-        #return AutoCommands.SimpleAuto(self.robotDrive)
- method to get the command to run during autonomous. 
-#The container will also set the default commands for the subsystems
-
- '''
-
-
-import commands2
-from commands2 import Command, CommandScheduler, cmd, InstantCommand, RunCommand, button
-import wpimath
-import wpilib
-from wpilib import XboxController
-
-from subsystems.coral_subsystem import CoralSubsystem
-from constants import AutoConstants, OIConstants, Setpoint
-from subsystems.drivetrain import DriveSubsystem
-from commands.SwerveJoystickCmd import SwerveJoystickCmd
-
-import commands.auto_routines
-
-
-class RobotContainer:
-    """
-    This class is where the bulk of the robot should be declared. Since Command-based is a
-    "declarative" paradigm, very little robot logic should actually be handled in the :class:`.Robot`
-    periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
-    subsystems, commands, and button mappings) should be declared here.
-    """
-
-    def __init__(self) -> None:
-
-
-        self.driver_controller = wpilib.XboxController(OIConstants.K_DRIVER_CONTROLLER_PORT)
-        self.operator_controller = wpilib.XboxController(OIConstants.K_OPERATOR_CONTROLLER_PORT)
-        # The robot's subsystems
-        self.robot_drive = DriveSubsystem()
-        self.coral = CoralSubsystem()
-        self.set_default_command()
-        self.configure_button_bindings()
-
-
-        # self.robotDrive.setDefaultCommand(
-        #     SwerveJoystickCmd(self.robotDrive, self.driverController)
-        # )
-
-        # # Configure default commands
-        # self.robotDrive.setDefaultCommand(
-        #     SwerveJoystickCmd(
-        #         robotDrive = self.robotDrive, driverController = self.driverController
-        #     )
-        # )
         
-        # Configure the button bindings
+        # OPERATOR Left Bumper -> Run Coral Intake
+        self.operator_controller.leftBumper().whileTrue(self.coral.run_intake_command())
+        # OPERATOR Right Bumper -> Run Coral Intake in Reverse
+        self.operator_controller.rightBumper().whileTrue(self.coral.reverse_intake_command()) 
 
-    def set_default_command (self) -> None:
+        # OPERATOR B Button -> Elevator/Arm to human player position
+        self.operator_controller.b().onTrue(self.coral.setSetpointCommand(Setpoint.K_CORAL_STATION))
+        # OPERATOR X Button -> Elevator/Arm to level 2 position
+        self.operator_controller.a().onTrue(self.coral.setSetpointCommand(Setpoint.K_LEVEL_2))
+        # OPERATOR Y Button -> Elevator/Arm to level 3 position
+        self.operator_controller.y().onTrue(self.coral.setSetpointCommand(Setpoint.K_LEVEL_3))
 
-        self.robot_drive.setDefaultCommand(
-            RunCommand(
-                lambda: self.robot_drive.drive(
-                    wpimath.applyDeadband(self.driver_controller.getLeftY() * -1, OIConstants.DEADZONE),
-                    wpimath.applyDeadband(self.driver_controller.getLeftX() * -1, OIConstants.DEADZONE),
-                    wpimath.applyDeadband(self.driver_controller.getRightX() * -1, OIConstants.DEADZONE),
-                    True
-                ),
-                self.robot_drive 
-            ) 
-        )
+        # DRIVER Start Button -> Zero swerve heading
+        self.driver_controller.start().onTrue(self.robot_drive.zero_heading)
+        
+        # DRIVER Y Button -> Lift robot using power only
+        self.driver_controller.y().onTrue(self.hang.lift_command_power)
+        # DRIVER X Button -> Lower robot using power only
+        self.driver_controller.x().onTrue(self.hang.lower_command_power)
 
-
-    def configure_button_bindings(self) -> None:
-        """
-        Use this method to define your button->command mappings. Buttons can be created by
-        instantiating a :GenericHID or one of its subclasses (Joystick or XboxController),
-        and then passing it to a JoystickButton.
-        # """
-        # self.robotDrive.setDefaultCommand(SwerveJoystickCmd(self.robotDrive, self.driverController))
-        if self.operator_controller.getLeftBumper(): CoralSubsystem.run_intake_command()
-        if self.operator_controller.getRightBumper(): CoralSubsystem.reverse_intake_command()   
-
-        if self.operator_controller.getBButton(): CoralSubsystem.setSetpointCommand(Setpoint.K_CORAL_STATION)
-        if self.operator_controller.getAButton(): CoralSubsystem.setSetpointCommand(Setpoint.K_LEVEL_2) 
-        if self.operator_controller.getYButton(): CoralSubsystem.setSetpointCommand(Setpoint.K_LEVEL_3) 
-
-        # if self.driver_controller.start(): self.robot_drive.zero_heading
+        # DRIVER B Button -> Lift robot using setpoint only
+        self.driver_controller.b().onTrue(self.hang.lift_to_setpoint)
+        # DRIVER A Button -> Lower robot using setpoint only
+        self.driver_controller.a().onTrue(self.hang.lower_to_setpoint)
 
 
     def disablePIDSubsystems(self) -> None:
@@ -193,7 +112,7 @@ class RobotContainer:
         This should be called on robot disable to prevent integral windup."""
         pass
 
-    def getAutonomousCommand(self) -> commands2.Command:
+    def getAutonomousCommand(self) -> Command:
         """Use this to pass the autonomous command to the main {@link Robot} class.
 
         :returns: the command to run in autonomous

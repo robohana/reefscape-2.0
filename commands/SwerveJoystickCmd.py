5395bd76e9fec9ae54ec6bcc1f7af36de2f2650a
Liliana <lilianacarlson.lrc@gmail.com>
2025-03-08 19:19:46 -0600

    Update SwerveJoystickCmd.py

--- commands/SwerveJoystickCmd.py
+++ commands/SwerveJoystickCmd.py
@@ -2,8 +2,8 @@ from constants import OIConstants, DriveConstants, RobotConstants
 from wpimath.filter import SlewRateLimiter
 from commands2 import Command 
 from wpilib import XboxController
-import wpimath
-from subsystems.drivesubsystem import DriveSubsystem
+from wpimath import applyDeadband
+from subsystems.drivetrain import DriveSubsystem
 from wpimath.kinematics import ChassisSpeeds
 from wpilib import SmartDashboard as sd
 
@@ -19,34 +19,28 @@ class SwerveJoystickCmd(Command):
         self.y_limiter = SlewRateLimiter(1)
         self.z_rot_limiter = SlewRateLimiter(1)
 
-        
-
     def initialize(self):
         pass
     
     def execute(self):
         # these are multiplied by the drivingSpeedLimiter which limit the speed of the robot so it doesn't go too fast
-        self.x_speed = self.driver_controller.getLeftY() #* DriveConstants.K_PHYSICAL_MAX_SPEED_METERS_PER_SECOND # self.drivingLimiter#* RobotConstants.kTeleopDriveMaxSpeedMetersPerSecond
-        self.y_speed = self.driver_controller.getLeftX() #* DriveConstants.K_PHYSICAL_MAX_SPEED_METERS_PER_SECOND # self.drivingLimiter #* RobotConstants.kTeleopDriveMaxSpeedMetersPerSecond
-        self.z_rotation = self.driver_controller.getRightX() #* DriveConstants.K_TELE_DRIVE_MAX_ANGULAR_SPEED_RADIANS_PER_SECOND # self.drivingLimiter
+        self.x_speed = self.driver_controller.getLeftY() 
+        self.y_speed = self.driver_controller.getLeftX() 
+        self.z_rotation = self.driver_controller.getRightX() 
         
         # 1. Get the joystick values and apply deadzone
-        self.x_speed = wpimath.applyDeadband(self.x_speed, OIConstants.DEADZONE)
-        self.y_speed = wpimath.applyDeadband(self.y_speed, OIConstants.DEADZONE)
-        self.z_rotation = wpimath.applyDeadband(self.z_rotation, OIConstants.DEADZONE)
+        self.x_speed = applyDeadband(self.x_speed, OIConstants.DEADZONE)
+        self.y_speed = applyDeadband(self.y_speed, OIConstants.DEADZONE)
+        self.z_rotation = applyDeadband(self.z_rotation, OIConstants.DEADZONE)
         
         # if self.fieldOriented:
         chasis_speeds = ChassisSpeeds.fromFieldRelativeSpeeds(self.x_speed, self.y_speed, self.z_rotation, self.robot_drive.get_rotation_2d())
         sd.putNumber("chasisSpeeds", chasis_speeds.vx)
-        # else:
-        # If robotOrinted is desired (But what's the fun in that?)
-        #     chasisSpeeds = ChassisSpeeds(self.xSpeed, self.ySpeed, self.zRotation)
 
         # 3. convert chasis speeds to module states
         module_states = RobotConstants.KINEMATICS.toSwerveModuleStates(chasis_speeds)
 
         self.robot_drive.set_module_states(module_states)
-        
 
     def end(self, interrupted: bool):
         self.robot_drive.stop()

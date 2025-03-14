8f568093b3db91037b73c679397a2f54fc47b6e7
Liliana <lilianacarlson.lrc@gmail.com>
2025-03-13 21:41:10 -0500

    Update move_to_coral_station_cmd.py

--- commands/move_to_coral_station_cmd.py
+++ commands/move_to_coral_station_cmd.py
@@ -1,6 +1,6 @@
 from commands2 import Command
 from subsystems.coral_subsystem import CoralSubsystem
-from constants import Setpoint
+from constants.constants import Setpoint
 from rev import SparkBase
 
 class MoveToCoralStation(Command):
@@ -13,8 +13,11 @@ class MoveToCoralStation(Command):
         pass
 
     def execute(self):
-        self.coral.arm_closed_loop_controller.setReference(Setpoint.Arm.K_CORAL_STATION, SparkLowLevel.ControlType.kMAXMotionPositionControl)
-        self.coral.elevator_closed_loop_controller.setReference(Setpoint.Elevator.K_CORAL_STATION, SparkLowLevel.ControlType.kMAXMotionPositionControl)
+        self.coral.arm_closed_loop_controller.setReference(Setpoint.Arm.K_CORAL_STATION, SparkBase.ControlType.kPosition)
+        self.coral.elevator_closed_loop_controller.setReference(Setpoint.Elevator.K_CORAL_STATION, SparkBase.ControlType.kPosition)
+        
+    def isFinished(self):
+        return True
 
     def end(self, interrupted):
         self.coral.stop_intake()

7493f551c716dec2dd1c202abbef88bdee4f3e6d
Liliana <lilianacarlson.lrc@gmail.com>
2025-03-13 20:28:54 -0500

    Create move_to_l1_cmd.py

---
+++ commands/move_to_l1_cmd.py
@@ -0,0 +1,28 @@
+from commands2 import Command, WaitUntilCommand
+from subsystems.coral_subsystem import CoralSubsystem
+from constants import Setpoint, CoralSubsystemConstants
+from commands.intake_cmd import ReleaseIntakeCommand, RunIntakeCommand, MoveToSetpointCommand
+
+from rev import SparkLowLevel, SparkBase
+
+class MoveToL1Command(Command):
+    def __init__(self, coral: CoralSubsystem):
+        super().__init__()
+        self.coral = coral
+        self.addRequirements(self.coral)
+
+    def initialize(self):
+        pass
+
+    def execute(self):
+        # self.coral.arm_closed_loop_controller.setReference(Setpoint.Arm.K_CORAL_STATION, SparkLowLevel.ControlType.kMAXMotionPositionControl)
+        # self.coral.elevator_closed_loop_controller.setReference(Setpoint.Elevator.K_CORAL_STATION, SparkLowLevel.ControlType.kMAXMotionPositionControl)
+
+        self.coral.arm_closed_loop_controller.setReference(Setpoint.Arm.K_LEVEL_1, SparkBase.ControlType.kPosition)
+        self.coral.elevator_closed_loop_controller.setReference(Setpoint.Elevator.K_LEVEL_1, SparkBase.ControlType.kPosition)
+        
+    def isFinished(self):
+        return True
+
+    def end(self, interrupted):
+        self.coral.stop_intake()

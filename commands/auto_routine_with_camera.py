418b4c7a4be62d1dd186216ec9ab154c59039357
Liliana <lilianacarlson.lrc@gmail.com>
2025-03-25 17:01:35 -0500

    Update auto_routine_with_camera.py

--- commands/auto_routine_with_camera.py
+++ commands/auto_routine_with_camera.py
@@ -6,7 +6,7 @@ from commands.drive_to_target import DriveToLimelightTarget
 from commands.coral_cmds import ScoreCoralL2
 
 class SimpleScoreAuto(SequentialCommandGroup):
-    def __init__(self, drivetrain: DriveSubsystem, coral: CoralSubsystem, target_tag_id: int):
+    def __init__(self, drivetrain: DriveSubsystem, coral: CoralSubsystem, target_tag_id: 22):
         """
         Autonomous routine that:
          1. Drives toward the scoring AprilTag (using vision with fallback),
@@ -21,8 +21,8 @@ class SimpleScoreAuto(SequentialCommandGroup):
         
         self.addCommands(
             # Drive to target using Limelight data
-            DriveToLimelightTarget(drivetrain, target_area_threshold=5.0, drive_speed=0.5, target_tag_id=target_tag_id),
-            InstantCommand(lambda: drivetrain.drive(0, 0, 0, True), drivetrain),
+            DriveToLimelightTarget(drivetrain, target_area_threshold=2.0, drive_speed=0.15, target_tag_id=target_tag_id),
+            InstantCommand(lambda: drivetrain.drive(0, 0, 0, False), drivetrain),
             # Score a coral at L2
             ScoreCoralL2(coral)
             

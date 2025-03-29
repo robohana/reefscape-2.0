20c3756640905bc4efc5295927df8a7538ce0047
Liliana <lilianacarlson.lrc@gmail.com>
2025-03-29 17:38:07 -0500

    Update coral_cmds.py

--- commands/coral_cmds.py
+++ commands/coral_cmds.py
@@ -2,19 +2,19 @@ from commands2 import SequentialCommandGroup
 from subsystems.coral_subsystem import CoralSubsystem
 from commands.intake_cmd import ReleaseIntakeCommand, RunIntakeCommand
 from commands.move_to_coral_station_cmd import MoveToCoralStation
-from commands.move_to_l1_cmd import MoveToL1Command
+from commands.move_to_zero_cmd import MoveToZeroCommand
 from commands.move_to_l2_cmd import MoveToL2Command
 from commands.move_to_l3_cmd import MoveToL3Command
 
-class ScoreCoralL1(SequentialCommandGroup):
+class IntakeToZero(SequentialCommandGroup):
     def __init__(self, coral: CoralSubsystem):
         super().__init__()
         self.addCommands(
             #Step 1: Move Elevator to L1 setpoint
             #Step 2: Move Arm to L1 Setpoint
-            MoveToL1Command(coral),
+            MoveToZeroCommand(coral)
             #Step 3: Release Intake Command. with wait
-            ReleaseIntakeCommand(coral)
+            # ReleaseIntakeCommand(coral)
         )
 
 class ScoreCoralL2(SequentialCommandGroup):
@@ -23,9 +23,9 @@ class ScoreCoralL2(SequentialCommandGroup):
         self.addCommands(
             #Step 1: Move Elevator to L2 setpoint
             #Step 2: Move Arm to L2 Setpoint
-            MoveToL2Command(coral),
+            MoveToL2Command(coral)
             #Step 3: Release Intake Command. with wait
-            ReleaseIntakeCommand(coral)
+            # ReleaseIntakeCommand(coral)
         )
 
 class ScoreCoralL3(SequentialCommandGroup):
@@ -34,9 +34,9 @@ class ScoreCoralL3(SequentialCommandGroup):
         self.addCommands(
             #Step 1: Move Elevator to L3 setpoint
             #Step 2: Move Arm to L3 Setpoint
-            MoveToL3Command(coral),
+            MoveToL3Command(coral)
             #Step 3: Release Intake Command. with wait
-            ReleaseIntakeCommand(coral)
+            # ReleaseIntakeCommand(coral)
         )   
 
 class IntakeCoralStation(SequentialCommandGroup):

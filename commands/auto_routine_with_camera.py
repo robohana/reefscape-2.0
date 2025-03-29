a37e4a0a87ad6133b1f6e9fbcc0366fff8c05deb
Liliana <lilianacarlson.lrc@gmail.com>
2025-03-29 17:33:45 -0500

    Update auto_routine_with_camera.py

--- commands/auto_routine_with_camera.py
+++ commands/auto_routine_with_camera.py
@@ -1,18 +1,20 @@
 # auto_routines.py
-from commands2 import SequentialCommandGroup, InstantCommand
+from commands2 import SequentialCommandGroup, InstantCommand, cmd, WaitCommand
 from subsystems.drivetrain import DriveSubsystem
 from subsystems.coral_subsystem import CoralSubsystem
 from commands.drive_to_target import DriveToLimelightTarget
-from commands.coral_cmds import ScoreCoralL3
+from commands.coral_cmds import ScoreCoralL2, IntakeToZero
 from commands.auto_align_scoring_cmd import AutoAlignScoringCommand
+from commands.auto_reverse_intake_cmd import AutoReverseIntakeCmd
 
-class SimpleScoreAuto(SequentialCommandGroup):
-    def __init__(self, drivetrain: DriveSubsystem, coral: CoralSubsystem, target_tag_id: 22):
+class SimpleScoreAutoLEFT(SequentialCommandGroup):
+    def __init__(self, drivetrain: DriveSubsystem, coral: CoralSubsystem, targetTag):
         """
         Autonomous routine that:
          1. Drives toward the scoring AprilTag (using vision with fallback),
          2. Scores at level 2,
          
+        
         :param drivetrain: DriveSubsystem instance.
         :param coral: CoralSubsystem instance.
         :param vision: VisionSystem instance.
@@ -22,9 +24,37 @@ class SimpleScoreAuto(SequentialCommandGroup):
         
         self.addCommands(
             # Drive to target using Limelight data
-            AutoAlignScoringCommand(drivetrain, 'left', 1.0, 1.0),
+            AutoAlignScoringCommand(drivetrain, 'left', 2.5, 1.25),
             InstantCommand(lambda: drivetrain.drive(0, 0, 0, False), drivetrain),
             # Score a coral at L2
-            ScoreCoralL3(coral)
-            
+            ScoreCoralL2(coral),
+            WaitCommand(1.5),
+            AutoReverseIntakeCmd(coral),
+            IntakeToZero(coral)
+        )
+
+class SimpleScoreAutoRIGHT(SequentialCommandGroup):
+    def __init__(self, drivetrain: DriveSubsystem, coral: CoralSubsystem, targetTag):
+        """
+        Autonomous routine that:
+         1. Drives toward the scoring AprilTag (using vision with fallback),
+         2. Scores at level 2,
+         
+        
+        :param drivetrain: DriveSubsystem instance.
+        :param coral: CoralSubsystem instance.
+        :param vision: VisionSystem instance.
+        :param score_tag_id: AprilTag ID for scoring.
+        """
+        super().__init__()
+        
+        self.addCommands(
+            # Drive to target using Limelight data
+            AutoAlignScoringCommand(drivetrain, 'right', 2.5, 1.25, targetTag),
+            InstantCommand(lambda: drivetrain.drive(0, 0, 0, False), drivetrain),
+            # Score a coral at L2
+            ScoreCoralL2(coral),
+            WaitCommand(1.5),
+            AutoReverseIntakeCmd(coral),
+            IntakeToZero(coral)
         )
